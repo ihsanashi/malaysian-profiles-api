4883f5e306a1edc8976942eb4c3da760d75697cc
Ahmad Ihsan <ihsanashi@gmail.com>
2022-07-11 19:40:01 +0800

    create entry point file in root directory

---
+++ index.js
@@ -0,0 +1,39 @@
+require('dotenv').config();
+
+const bodyParser = require('body-parser');
+const cors = require('cors');
+
+const express = require('express');
+const app = express();
+
+const PORT = process.env.PORT || 3000;
+
+const usersRoute = require('./src/routes/users');
+
+// Middlewares
+app.use(cors());
+app.use(bodyParser.json());
+app.use(
+  bodyParser.urlencoded({
+    extended: true,
+  })
+);
+
+// Routes
+app.get('/', (req, res) => {
+  res.json({ msg: 'Oops, nothing to see here... Move along.' });
+});
+
+app.use('/api/users', usersRoute);
+
+// Handle undefined routes
+app.all('*', (req, res) =>
+  res.json({
+    msg: 'This route does not exist.',
+  })
+);
+
+// Start server
+app.listen(PORT, () => {
+  console.log(`Server listening on port ${PORT}`);
+});

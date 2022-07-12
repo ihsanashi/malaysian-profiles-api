258dca490edf7ced6ade707d31e2d7712974d208
Ahmad Ihsan <ihsanashi@gmail.com>
2022-07-12 17:15:15 +0800

    update sql queries with additional details

--- src/models/database.sql
+++ src/models/database.sql
@@ -1,5 +1,14 @@
 CREATE DATABASE malaysian_profiles;
 
-CREATE TABLE users (id SERIAL PRIMARY KEY, email VARCHAR(128));
+CREATE TABLE users (
+  id SERIAL PRIMARY KEY,
+  email VARCHAR(254) NOT NULL,
+  created_at timestamp with time zone default CURRENT_TIMESTAMP NOT NULL,
+  updated_at timestamp with time zone
+);
 
-CREATE TABLE profiles (id SERIAL PRIMARY KEY, name VARCHAR(256));
+CREATE TABLE profiles (
+  id SERIAL PRIMARY KEY,
+  name VARCHAR(256) NOT NULL,
+  created_at timestamp with time zone default CURRENT_TIMESTAMP NOT NULL,
+);

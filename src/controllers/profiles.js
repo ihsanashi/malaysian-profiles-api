151ae4cc17406538a0f6c47af34d2f270e14959f
Ahmad Ihsan <ihsanashi@gmail.com>
2022-07-16 13:00:09 +0800

    feat: display addresses in the results of profiles array or profile object, handle creating, updating and deleting profile

--- src/controllers/profiles.js
+++ src/controllers/profiles.js
@@ -2,11 +2,33 @@ const pool = require('../models/index');
 
 const getAllProfiles = async (req, res) => {
   try {
-    const profilesWithAddresses = await pool.query(
-      'SELECT * FROM profiles LEFT JOIN addresses ON profiles.id = addresses.profile_id ORDER BY profiles.created_at'
+    const profileRecords = await pool.query(
+      'SELECT * FROM profiles ORDER BY profiles.created_at;'
     );
 
-    res.status(200).json(profilesWithAddresses.rows);
+    const profileIds = [];
+
+    profileRecords.rows.forEach((profile) => {
+      profileIds.push(profile.id);
+    });
+
+    const addressesRecords = await pool.query(
+      'SELECT * FROM addresses WHERE profile_id = ANY ($1);',
+      [profileIds]
+    );
+
+    const profiles = [];
+
+    profileRecords.rows.forEach((profile) => {
+      const addresses = addressesRecords.rows.filter(
+        (address) => address.profile_id === profile.id
+      );
+
+      profile.addresses = addresses;
+      profiles.push(profile);
+    });
+
+    res.status(200).send(profiles);
   } catch (err) {
     console.error(err.message);
   }
@@ -14,6 +36,25 @@ const getAllProfiles = async (req, res) => {
 
 const getProfile = async (req, res) => {
   try {
+    const { profile_id } = req.params;
+    const profileRecord = await pool.query(
+      'SELECT * FROM profiles WHERE id = $1;',
+      [profile_id]
+    );
+
+    const addressRecords = await pool.query(
+      'SELECT * FROM addresses WHERE profile_id = $1;',
+      [profile_id]
+    );
+
+    const profile = profileRecord.rows[0];
+
+    if (profileRecord.rows.length === 0 && addressRecords.rows.length === 0) {
+      res.status(404).json({ message: 'The requested resource was not found' });
+    } else {
+      profile.addresses = addressRecords.rows;
+      res.status(200).json(profile);
+    }
   } catch (err) {
     console.error(err.message);
   }
@@ -21,6 +62,22 @@ const getProfile = async (req, res) => {
 
 const createProfile = async (req, res) => {
   try {
+    const {
+      user_id,
+      first_name,
+      last_name,
+      email,
+      birthdate,
+      gender,
+      is_deletable,
+    } = req.body;
+
+    const profile = await pool.query(
+      'INSERT INTO profiles (user_id, first_name, last_name, email, birthdate, gender, is_deletable) VALUES ($1, $2, $3, $4, $5, $6, $7) RETURNING *;',
+      [user_id, first_name, last_name, email, birthdate, gender, is_deletable]
+    );
+
+    res.status(201).json(profile.rows[0]);
   } catch (err) {
     console.error(err.message);
   }
@@ -28,6 +85,24 @@ const createProfile = async (req, res) => {
 
 const updateProfile = async (req, res) => {
   try {
+    const { profile_id } = req.params;
+    const { first_name, last_name, email, birthdate, gender, is_deletable } =
+      req.body;
+
+    const updateProfile = await pool.query(
+      'UPDATE profiles SET first_name = $2, last_name = $3, email = $4, birthdate = $5, gender = $6, is_deletable = $7, updated_at = CURRENT_TIMESTAMP WHERE id = $1 RETURNING *;',
+      [
+        profile_id,
+        first_name,
+        last_name,
+        email,
+        birthdate,
+        gender,
+        is_deletable,
+      ]
+    );
+
+    res.status(200).json(updateProfile.rows[0]);
   } catch (err) {
     console.error(err.message);
   }
@@ -35,6 +110,14 @@ const updateProfile = async (req, res) => {
 
 const deleteProfile = async (req, res) => {
   try {
+    const { profile_id } = req.params;
+
+    const deleteProfile = await pool.query(
+      'DELETE FROM profiles WHERE id = $1 RETURNING *;',
+      [profile_id]
+    );
+
+    res.status(204).send();
   } catch (err) {
     console.error(err.message);
   }
